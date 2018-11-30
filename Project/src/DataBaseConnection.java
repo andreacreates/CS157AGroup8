0abc65754746130e9de2fc4ab7cef73a143dea48
Justin Ocampo <lingling287@gmail.com>
2018-11-30 13:42:02 -0800

    added class to connecting to db

---
+++ Project/src/DataBaseConnection.java
@@ -0,0 +1,91 @@
+import java.sql.*;
+
+
+// Class used for connecting to the Database
+public class DataBaseConnection {
+	// JDBC driver name and database URL
+	private final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+	private final String DB_URL = "jdbc:mysql://35.233.153.166:3306/cs157";
+
+	// Database credentials
+	private final String USER = "root";
+	private final String PASS = "group3cs160";
+	
+	// SQL Objects
+	private Connection conn;
+	private Statement stmt;
+	private ResultSet rs;
+
+	public Connection openConnection() {
+		try {
+			Class.forName(JDBC_DRIVER);
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connected!!");
+			return conn;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	// creates prepared statement
+	public PreparedStatement createPreparedStatement(String sql) {
+		try {
+			stmt = conn.prepareStatement(sql);
+			return (PreparedStatement) stmt;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	// executes prepared statement and returns result set
+	public ResultSet executePreparedStatement() {
+		PreparedStatement ps = (PreparedStatement) stmt;
+		try {
+			rs =  ps.executeQuery();
+			return rs;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// executes regular statement and returns result set
+	public ResultSet sqlStatement(String sql) {
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			return rs;
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	// closes all SQL objects
+	public void closeConnection() {
+		try {
+			if (stmt != null)
+				stmt.close();
+		} catch (SQLException se2) {
+		}
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+
+		try {
+			if (stmt != null)
+				stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- Project/src/HelloWorld.java
+++ Project/src/HelloWorld.java
@@ -1,6 +1,101 @@
+import java.sql.*;
 
+// Example Class how to use JDBC with DataBaseConnection class I created
 public class HelloWorld {
+
+	public static void statementExample() {
+		DataBaseConnection db = new DataBaseConnection();
+		db.openConnection();
+		try {
+			System.out.println("Model\tBrand\tType");
+
+			ResultSet rs = db.sqlStatement("SELECT * FROM product");
+
+		
+			while (rs.next()) {
+				// Retrieve by column name
+				int model = rs.getInt("model");
+				String brand = rs.getString("brand");
+				String type = rs.getString("type");
+
+				// Display values
+				System.out.print(model);
+				System.out.print("\t" + brand);
+				System.out.println("\t" + type);
+			}
+
+			db.closeConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void preparedStatementExample() {
+		DataBaseConnection db = new DataBaseConnection();
+		db.openConnection();
+		
+		// Parameters are (?)
+		PreparedStatement stmt = db.createPreparedStatement("INSERT INTO user VALUES(?,?,?,?)");
+		try {
+			
+			// Sets parameters of (?) SQL statement 
+			stmt.setInt(1, 25);
+			stmt.setString(2, "M");
+			stmt.setInt(3, 8);
+			stmt.setInt(4, 80);
+			
+			
+			// use executeUpdate when calling INSERT, UPDATE, or DELETE only
+			stmt.executeUpdate();
+			
+			db.closeConnection();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	public static void argumentExample(String input) {
+	
+		DataBaseConnection db = new DataBaseConnection();
+		db.openConnection();
+		
+		PreparedStatement stmt = db.createPreparedStatement("SELECT * FROM product WHERE type = ?");
+		
+		try {
+			stmt.setString(1, input);
+			
+			ResultSet rs = db.executePreparedStatement();
+			
+			while (rs.next()) {
+				// Retrieve by column name
+				int model = rs.getInt("model");
+				String brand = rs.getString("brand");
+				String type = rs.getString("type");
+
+				// Display values
+				System.out.print(model);
+				System.out.print("\t" + brand);
+				System.out.println("\t" + type);
+			}
+
+			db.closeConnection();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+
+		HelloWorld.argumentExample("percussion");	
+		//HelloWorld.statementExample();
+		//HelloWorld.preparedStatementExample();
+		
+		System.out.println("Goodbye!");
 	}
 }
